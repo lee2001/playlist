bcaa7cf5c8e4f2fb3d6b4fe60d72c2b8a66b8c63
Leandra Aikins <leandra.aikins@gmail.com>
2019-01-08 23:18:53 +0000

    new code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Aye","I'm In Love With You","Best in me","What can I do?"];
+var artists = ["Davido","Eyfa","Marvin Sapp","Tye Tribbett"];
+var imagesLinks = ["http://tooxclusive.com/wp-content/uploads/2014/02/Davido-Aye-Screenshot.jpg","https://www.bellanaija.com/wp-content/uploads/2012/02/Sarkodie-Efya.jpg","https://i.ytimg.com/vi/U-UAP_LMpqc/hqdefault.jpg","https://i.ytimg.com/vi/Oe9qRXe9yAc/hqdefault.jpg"];
+var songLength = ["412","518","834","743"];
+var songLink = ["https://www.youtube.com/watch?v=uZ-_HIoEBE8","https://www.youtube.com/watch?v=j-T42ulYMcU","https://www.youtube.com/watch?v=U-UAP_LMpqc","https://www.youtube.com/watch?v=Oe9qRXe9yAc"];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
